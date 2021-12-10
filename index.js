8ac84b10f9a69a074ba1eb2a0afb2cce053a6c05
agocs6921 <47692733+agocs6921@users.noreply.github.com>
2021-12-10 21:40:07 +0100

    First commit

---
+++ index.js
@@ -0,0 +1,72 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const fs = require("fs");
+const { stdout } = require("process");
+const io = require("socket.io")(3000, {
+    cors: {
+        origin: '*'
+    }
+});
+
+const app = express();
+const PORT = 80;
+
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({extended: true}));
+app.use("/static", express.static("static"));
+
+app.set("view engine", "ejs");
+
+const MSGFILENAME = "data/messages.json";
+/*
+[
+    {
+        alias: "John Doe",
+        message: "amogus"
+    }
+]
+*/
+var messages = [];
+
+function addMessage(alias, message) {
+    let to_send = JSON.stringify({alias, message}) + "\n"
+
+    try {
+        fs.appendFileSync(MSGFILENAME, to_send)
+    } catch(err) {
+        fs.writeFileSync(MSGFILENAME, to_send)
+    }
+}
+
+function loadMessages() {
+    try {
+        String(fs.readFileSync(MSGFILENAME)).split("\n").map(v => {
+            if (v) {
+                messages.push(JSON.parse(v));
+            }
+        })
+    } catch(err) {
+        stdout.write("Can't load messages\n")
+        stdout.write(String(err) + "\n")
+    }
+}
+
+io.on("connect", (socket) => {
+    stdout.write(`${socket.id} connected\n`)
+
+    socket.on("message", (alias, message) => {
+        addMessage(alias, message);
+    
+        socket.broadcast.emit("message-received", alias, message);
+    })
+})
+
+
+app.get("/", (req, res) => {    
+    res.render("index", { messages });
+})
+
+app.listen(PORT, () => {
+    loadMessages();
+    stdout.write("Ready.\n")
+})
